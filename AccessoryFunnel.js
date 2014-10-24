d677bd17b8670e339fbd18e97aa11f0846e139c9
Roy <roy.arents@nlr.nl>
2014-10-24 19:03:47 +0000

    Seperated accessory in seperate file

---
+++ AccessoryFunnel.js
@@ -0,0 +1,134 @@
+var characteristic_Factor = new require("./Characteristic.js");
+
+var sys = require('sys');
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) };
+
+module.exports = {
+  generateLightService: function (service_Factor) {
+	var lightService = new service_Factor.Service("00000043-0000-1000-8000-0026BB765291");
+
+	var nameOptions = {
+		type: "00000023-0000-1000-8000-0026BB765291",
+		perms: [
+			"pr"
+		],
+		format: "string",
+		initialValue: "Light 1",
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255,
+	}
+	var nameChar = new characteristic_Factor.Characteristic(nameOptions);
+	lightService.addCharacteristic(nameChar);
+
+	var onOptions = {
+		type: "00000025-0000-1000-8000-0026BB765291",
+		perms: [
+			"pw",
+			"pr",
+			"ev"
+		],
+		format: "bool",
+		initialValue: false,
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Turn On the Light",
+		designedMaxLength: 1,
+	}
+	var lightSwitchChar = new characteristic_Factor.Characteristic(onOptions, function(value) {
+		console.log("Light Status Change:",value);
+
+                var lightcmd = "lightwaverf Woonkamer Funnel ";
+                if(value) lightcmd = lightcmd + "on";
+                else lightcmd = lightcmd + "off";
+                console.log(lightcmd);
+                exec(lightcmd);
+	});
+	lightService.addCharacteristic(lightSwitchChar);
+
+	return lightService;
+  },
+  generateAccessoryInfoService: function (service_Factor, name, model, sn, manufacturer) {
+	var infoService = new service_Factor.Service("0000003E-0000-1000-8000-0026BB765291");
+
+	var nameOptions = {
+		type: "00000023-0000-1000-8000-0026BB765291",
+		perms: [
+			"pr"
+		],
+		format: "string",
+		initialValue: name,
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255,
+	}
+	var nameChar = new characteristic_Factor.Characteristic(nameOptions);
+	infoService.addCharacteristic(nameChar);
+
+	var manufacturerOptions = {
+		type: "00000020-0000-1000-8000-0026BB765291",
+		perms: [
+			"pr"
+		],
+		format: "string",
+		initialValue: manufacturer,
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255,
+	}
+	var manufacturerChar = new characteristic_Factor.Characteristic(manufacturerOptions);
+	infoService.addCharacteristic(manufacturerChar);
+
+	var modelOptions = {
+		type: "00000021-0000-1000-8000-0026BB765291",
+		perms: [
+			"pr"
+		],
+		format: "string",
+		initialValue: model,
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255,
+	}
+	var modelChar = new characteristic_Factor.Characteristic(modelOptions);
+	infoService.addCharacteristic(modelChar);
+
+	var snOptions = {
+		type: "00000030-0000-1000-8000-0026BB765291",
+		perms: [
+			"pr"
+		],
+		format: "string",
+		initialValue: sn,
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255,
+	}
+	var snChar = new characteristic_Factor.Characteristic(snOptions);
+	infoService.addCharacteristic(snChar);
+
+	var identifyOptions = {
+		type: "00000014-0000-1000-8000-0026BB765291",
+		perms: [
+			"pw"
+		],
+		format: "bool",
+		initialValue: false,
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Identify Accessory",
+		designedMaxLength: 1,
+	}
+	var identifyChar = new characteristic_Factor.Characteristic(identifyOptions);
+	infoService.addCharacteristic(identifyChar);
+
+	return infoService;
+  }
+};
+
--- Core.js
+++ Core.js
@@ -5,148 +5,27 @@ var accessoryController_Factor = new require("./AccessoryController.js");
 var service_Factor = new require("./Service.js");
 var characteristic_Factor = new require("./Characteristic.js");
 
+var accessoryFunnel = new require("./AccessoryFunnel.js")
+
 var sys = require('sys');
-//var lightwaverf = require("child_process").exec('lightwaverf').unref();
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { sys.puts(stdout) };
 
 
 console.log("HAP-NodeJS starting...");
 
+
+//var lightcmd = "python /home/xbian/pyharmony/harmony turn_off";
+//console.log(lightcmd);
+//exec(lightcmd);
+
 storage.initSync();
 
 var accessoryController = new accessoryController_Factor.AccessoryController();
-var infoService = generateAccessoryInfoService("Test Accessory 1","Rev 1","A1S2NASF88EW","Nlr");
-var lightService = generateLightService();
+var infoService = accessoryFunnel.generateAccessoryInfoService(service_Factor,"Test Accessory 1","Rev 1","A1S2NASF88EW","Nlr");
+var lightService = accessoryFunnel.generateLightService(service_Factor);
 accessoryController.addService(infoService);
 accessoryController.addService(lightService);
 
 var accessory = new accessory_Factor.Accessory("Test Accessory 1", "1A:2B:3C:4D:5E:FF", storage, parseInt(51822), "031-45-154", accessoryController);
 accessory.publishAccessory();
-
-function generateLightService() {
-	var lightService = new service_Factor.Service("00000043-0000-1000-8000-0026BB765291");
-
-	var nameOptions = {
-		type: "00000023-0000-1000-8000-0026BB765291",
-		perms: [
-			"pr"
-		],
-		format: "string",
-		initialValue: "Light 1",
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Bla",
-		designedMaxLength: 255,
-	}
-	var nameChar = new characteristic_Factor.Characteristic(nameOptions);
-	lightService.addCharacteristic(nameChar);
-
-	var onOptions = {
-		type: "00000025-0000-1000-8000-0026BB765291",
-		perms: [
-			"pw",
-			"pr",
-			"ev"
-		],
-		format: "bool",
-		initialValue: false,
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Turn On the Light",
-		designedMaxLength: 1,
-	}
-	var lightSwitchChar = new characteristic_Factor.Characteristic(onOptions, function(value) {
-		console.log("Light Status Change:",value);
-
-                var lightcmd = "lightwaverf Woonkamer Funnel ";
-                if(value) lightcmd = lightcmd + "on";
-                else lightcmd = lightcmd + "off";
-                console.log(lightcmd);
-                exec(lightcmd);
-	});
-	lightService.addCharacteristic(lightSwitchChar);
-
-	return lightService;
-}
-
-function generateAccessoryInfoService(name, model, sn, manufacturer) {
-	var infoService = new service_Factor.Service("0000003E-0000-1000-8000-0026BB765291");
-
-	var nameOptions = {
-		type: "00000023-0000-1000-8000-0026BB765291",
-		perms: [
-			"pr"
-		],
-		format: "string",
-		initialValue: name,
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Bla",
-		designedMaxLength: 255,
-	}
-	var nameChar = new characteristic_Factor.Characteristic(nameOptions);
-	infoService.addCharacteristic(nameChar);
-
-	var manufacturerOptions = {
-		type: "00000020-0000-1000-8000-0026BB765291",
-		perms: [
-			"pr"
-		],
-		format: "string",
-		initialValue: manufacturer,
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Bla",
-		designedMaxLength: 255,
-	}
-	var manufacturerChar = new characteristic_Factor.Characteristic(manufacturerOptions);
-	infoService.addCharacteristic(manufacturerChar);
-
-	var modelOptions = {
-		type: "00000021-0000-1000-8000-0026BB765291",
-		perms: [
-			"pr"
-		],
-		format: "string",
-		initialValue: model,
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Bla",
-		designedMaxLength: 255,
-	}
-	var modelChar = new characteristic_Factor.Characteristic(modelOptions);
-	infoService.addCharacteristic(modelChar);
-
-	var snOptions = {
-		type: "00000030-0000-1000-8000-0026BB765291",
-		perms: [
-			"pr"
-		],
-		format: "string",
-		initialValue: sn,
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Bla",
-		designedMaxLength: 255,
-	}
-	var snChar = new characteristic_Factor.Characteristic(snOptions);
-	infoService.addCharacteristic(snChar);
-
-	var identifyOptions = {
-		type: "00000014-0000-1000-8000-0026BB765291",
-		perms: [
-			"pw"
-		],
-		format: "bool",
-		initialValue: false,
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Identify Accessory",
-		designedMaxLength: 1,
-	}
-	var identifyChar = new characteristic_Factor.Characteristic(identifyOptions);
-	infoService.addCharacteristic(identifyChar);
-
-	return infoService;
-}
